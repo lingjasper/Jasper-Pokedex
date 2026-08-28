0f77768ad30fe66b6d4b6ba6cce0ac4ad2bd8beb
Jasper Ling <39891617+lingjasper@users.noreply.github.com>
2026-08-27 23:40:31 -0700

    Beta v0.13.5.5 - Update release moniker

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 (() => {
   'use strict';
   // SINGLE SOURCE OF TRUTH for the website release moniker.
-  window.JASPER_POKEDEX_VERSION = 'Beta v0.13.5.4';
+  window.JASPER_POKEDEX_VERSION = 'Beta v0.13.5.5';
 })();
