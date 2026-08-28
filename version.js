9c3028dc8d07df1dee0ab5f4f7623b9f48dc2464
Jasper Ling <39891617+lingjasper@users.noreply.github.com>
2026-08-28 00:44:11 -0700

    RC 3 (Beta v0.13.5.8) - Update release moniker

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 (() => {
   'use strict';
   // SINGLE SOURCE OF TRUTH for the website release moniker.
-  window.JASPER_POKEDEX_VERSION = 'RC 3 (Beta v0.13.5.7)';
+  window.JASPER_POKEDEX_VERSION = 'RC 3 (Beta v0.13.5.8)';
 })();
