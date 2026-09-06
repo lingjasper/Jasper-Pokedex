04e808e005ebf28323b2cd17b1b68fba23bcb7a5
Jasper Ling <39891617+lingjasper@users.noreply.github.com>
2026-09-05 19:50:49 -0700

    Update JASPER_POKEDEX_VERSION to v1.0.1

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 (() => {
   'use strict';
   // SINGLE SOURCE OF TRUTH for the website release moniker.
-  window.JASPER_POKEDEX_VERSION = 'Release v1.0.0';
+  window.JASPER_POKEDEX_VERSION = 'Release v1.0.1';
 })();
